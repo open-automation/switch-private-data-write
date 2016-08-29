50769f2c65c251ff5a7c06c33fb4237c1b6f1d8e
Dominick Peluso <peluso.dominick@gmail.com>
2016-08-29 15:48:59 -0400

    Add source copy

--- Unpacked/WriteToPrivateData/WriteToPrivateData.js
+++ Unpacked/WriteToPrivateData/WriteToPrivateData.js
@@ -1,1 +1,36 @@
-tEDlwDUivglOH2u/SRLHnLVhTseBAMQsaFMpp2ff4RUa2nRLay/2ocGi8SbS+XHSDxH33Kv1gLzmvRGzABknTCzuvYqsSbk06I/TO/05v+IeO6mzeQSNTKv0+E8f2ZwiEew7FZILTfDi7ObIRtB+wDrv0zxa+jnGg/3hTWCUWGDQKCw1BCIakeSg5cnZW2rFp9HPTDJPDi7YPXYANtmXCkef0gYrOct3dl0t+AG/W+NCG4UihFskZSCgM3WKUL9rLcs3eOfZP31+rgVMzx7J8SgvWhCPN6ALQq0TDJeRIkYZy23/41BaDNundDW0A2GrT1036trMVO/aOTfNnVNTj1o0ngQWo8LYnUBXq8esqcHnFeQObspGkzduCIH+lfYTCF29wEZM6rqiFhTOv2FUsiHxp9SIf9GLaQovc/DuNR7aP0kFQU0i3lGSh/YEYlKsbX+K1F7dyOp6pMKJw/IOwoof0XHn+KLYj6i7eeAVefGhakfdGw0vDnxDQ9ZkiNuly2ssvKzI5VsToM46e9mlaxVnn0amEpbCOfCUR7rGbUH74P9qh6LlyKJnKppZUogCLkftf+wHKOUZX3FAnw+Qq2HguRCJuFrpDCiMtrikJHjoXei1sW3Jouzain2VkKSiQe1sG5egTCTNcLygxOw3/FOGS6Uxu5iEL7TsiOJVVamGoQ7/MHY7XyxeIsrjlsyb3/6m0hhq5rnXliZ94+zoqfzUqAOM+QIZ5FmOS44Cv2PZ0NjAbJ//LITy42rX/SUMT9FFrvCOgHevMwbfus8lCv+U/J54SqxEWIaLPrV6Y4YfO/jre6qR2BhBXQEZixgww3K0BJPagY300MobgvbbdqcKdsntgBNUu6HGSI0ySzujbM3rvHN0IMHrwjUnGNlPfQw60MJsOzT/Ho8pnsEV/SadARnbmKVYblGBt3/8+kZU5Me+90JDpmgBnp7D/d/JyMvY+tPSR0/kqZ47Wa7D6UCcq3y01iAnDXDKWMTPgKB6Ld+bZTGcUTeCCsbvALkhCxJvcgK5k8ADPYsrqpS57qhspSiseeAhU08J4faReNDGjzR+Tm2QFykpkwDzuH2vymraLHdafN6idik7RGEwet4MF+ExC6TGPJ24eslBxx1yedOo4ezhoUHLtQOA/Kfe2VVNIb2KFZHBp2ZHJSqhryoQrZg4seUm8qDTjsjQ3xDx6e+m/yHBJ4KWaVmbtZ8zoHbRl9wEnYDh3DQzc9TYHX8xpWFvCI6h8D+udxU+1iY=
+function jobArrived( s : Switch, job : Job )
+{
+	// Debugging
+	var debugWarnings = s.getPropertyValue( "DebugWarnings" );
+	var logLevel = -1;
+	if(debugWarnings == "Yes"){
+		logLevel = 2;
+	}
+
+	// PD helper function
+	var setKeyValuePD = function(keyProperty, valueProperty){
+		var key = s.getPropertyValue( keyProperty );
+		var value = s.getPropertyValue( valueProperty );
+
+		if(key){
+			job.setPrivateData( key, value );
+			s.log(logLevel, keyProperty + ': "' + key + '" => "' + value + '"');
+		}
+	}
+
+	// Do it
+	setKeyValuePD("Key1", "Value1");
+	setKeyValuePD("Key2", "Value2");
+	setKeyValuePD("Key3", "Value3");
+	setKeyValuePD("Key4", "Value4");
+	setKeyValuePD("Key5", "Value5");
+	setKeyValuePD("Key6", "Value6");
+	setKeyValuePD("Key7", "Value7");
+	setKeyValuePD("Key8", "Value8");
+	setKeyValuePD("Key9", "Value9");
+	setKeyValuePD("Key10", "Value10");
+
+	// Send to single
+	job.sendToSingle( job.getPath() );
+
+}
